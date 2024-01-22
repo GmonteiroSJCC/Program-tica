8c2b8fd2d288f3646a4ae26924367e05805e9bba
GmonteiroSJCC <gmonteiro@sjcc.com.br>
2024-01-22 10:41:03 -0300

    acompanhamento diario
    Terminamos

---
+++ diario/relacaoblocoportal.py
@@ -0,0 +1,103 @@
+parceiros = {
+    "Portal NE10" :
+    ["parceiros (10869286) » ne_10 (11238766)",    "parceiros (10869286) » ne_10 (11238766) » amp_stories (22722284121)",
+    "parceiros (10869286) » ne_10 (11238766) » anchor (22834406532)",    "parceiros (10869286) » ne_10 (11238766) » audio (22958379895)",
+    "parceiros (10869286) » ne_10 (11238766) » entretenimento (21722649521)",    "parceiros (10869286) » ne_10 (11238766) » educacao (22500047431) » in-article (22501462404)",
+    "parceiros (10869286) » ne_10 (11238766) » home (21723566961)",    "parceiros (10869286) » ne_10 (11238766) » home (21723566961) » anchor (22835570053)",
+    "parceiros (10869286) » ne_10 (11238766) » home (21723566961) » in-article (21814819361)",    "parceiros (10869286) » ne_10 (11238766) » home (21723566961) » in-article (21814819361) » outstream (22691595912)",
+    "parceiros (10869286) » ne_10 (11238766) » in-article (21720229610)",    "parceiros (10869286) » ne_10 (11238766) » indica (22674207097)",
+    "parceiros (10869286) » ne_10 (11238766) » jornal_do_commercio (21268726) » blog (21725415585) » oviral (21725355524) » in-article (21823751460)",
+    "parceiros (10869286) » ne_10 (11238766) » jornal_do_commercio (21268726) » blog (21725415585) » o-viral (22667687894)",
+    "parceiros (10869286) » ne_10 (11238766) » noticias (21722702436)",    "parceiros (10869286) » ne_10 (11238766) » noticias (21722702436) » amp_stories (22937541633)",
+    "parceiros (10869286) » ne_10 (11238766) » noticias (21722702436) » anchor (22835337900)",    "parceiros (10869286) » ne_10 (11238766) » noticias (21722702436) » in-article (22521807661)",
+    "parceiros (10869286) » ne_10 (11238766) » noticias (21722702436) » in-article (22521807661) » outstream (22691595927)",
+    "parceiros (10869286) » ne_10 (11238766) » tvcoin (23001242885)",    "parceiros_mobile (12014326) » ne_10 (21240886)",
+    "parceiros (10869286) » ne_10 (11238766) » produtos (21825267079) » conexaosaude (22480302082)",    "parceiros (10869286) » ne_10 (11238766) » produtos (21825267079) » saojoao2019 (21825267034)",
+    "parceiros (10869286) » ne_10 (11238766) » blog (21722649674)",    "parceiros (10869286) » ne_10 (11238766) » jornal_do_commercio (21268726) » blog (21725415585) » oviral (21725355524)",
+    "parceiros (10869286) » ne_10 (11238766) » produtos (21825267079)",    "parceiros (10869286) » ne_10 (11238766) » esportes (21722702850)"],
+
+    "Interior":
+    ["parceiros (10869286) » ne_10 (11238766) » interior (21737767482)",    "parceiros (10869286) » ne_10 (11238766) » interior (21737767482) » amp_stories (22722750494)",
+    "parceiros (10869286) » ne_10 (11238766) » interior (21737767482) » anchor (22834637125)",    "parceiros (10869286) » ne_10 (11238766) » interior (21737767482) » in-article (21823750911)",
+    "parceiros (10869286) » ne_10 (11238766) » interior (21737767482) » in-article (21823750911) » outstream (22691595915)",    "parceiros (10869286) » ne_10 (11238766) » interior (21737767482) » produtos (22024412677) » trofeu (22024507104)",
+    "parceiros (10869286) » ne_10 (11238766) » interior (21737767482) » radio_jornal (22042866649) » caruaru (22042962197) » video (22683744243)",
+    "parceiros (10869286) » ne_10 (11238766) » interior (21737767482) » radio_jornal (22042866649) » garanhuns (22042866655) » video (22684272305)",
+    "parceiros (10869286) » ne_10 (11238766) » interior (21737767482) » radio_jornal (22042866649) » petrolina (22042962203) » video (22684270175)",
+    "parceiros (10869286) » ne_10 (11238766) » interior (21737767482) » radio_jornal (22042866649) » pesqueira (22042866652) » video (22684272350)",
+    "parceiros (10869286) » ne_10 (11238766) » tv_jornal_interior (21743681688)",    "parceiros (10869286) » ne_10 (11238766) » tv_jornal_interior (21743681688) » video (22684354228)",
+    "parceiros (10869286) » ne_10 (11238766) » interior (21737767482) » story (22870966944)",    "parceiros (10869286) » ne_10 (11238766) » interior (21737767482) » produtos (22024412677) » comidas_gigantes (22024442840)",
+    "parceiros (10869286) » ne_10 (11238766) » interior (21737767482) » produtos (22024412677)",
+    "parceiros (10869286) » ne_10 (11238766) » interior (21737767482) » radio_jornal (22042866649) » limoeiro (22042866658) » video (22684272344)"],
+
+    "Jamildo":
+    ["parceiros (10869286) » ne_10 (11238766) » jornal_do_commercio (21268726) » colunas (22678504715) » jamildo (22678108642)",
+    "parceiros (10869286) » ne_10 (11238766) » jornal_do_commercio (21268726) » blog (21725415585) » jamildo (22475149105)",
+    "parceiros (10869286) » ne_10 (11238766) » jornal_do_commercio (21268726) » blog (21725415585) » jamildo (22475149105) » anchor (22835571571)",
+    "parceiros (10869286) » ne_10 (11238766) » jornal_do_commercio (21268726) » blog (21725415585) » jamildo (22475149105) » in-article (22475135310)"],
+
+    "JC Online":
+    ["parceiros (10869286) » ne_10 (11238766) » jornal_do_commercio (21268726)",
+    "parceiros (10869286) » ne_10 (11238766) » jornal_do_commercio (21268726) » coluna_toques (22219892338)",
+    "parceiros (10869286) » ne_10 (11238766) » jornal_do_commercio (21268726) » amp_stories (22696598094)",
+    "parceiros (10869286) » ne_10 (11238766) » jornal_do_commercio (21268726) » anchor (22834992269)",
+    "parceiros (10869286) » ne_10 (11238766) » jornal_do_commercio (21268726) » blog (21725415585) » meupet (22672700745) » in-article (22673286989)",
+    "parceiros (10869286) » ne_10 (11238766) » jornal_do_commercio (21268726) » blog (21725415585) » turismodevalor (22672701000) » in-article (22673287160)",
+    "parceiros (10869286) » ne_10 (11238766) » jornal_do_commercio (21268726) » in-article (21814348402)",
+    "parceiros (10869286) » ne_10 (11238766) » jornal_do_commercio (21268726) » in-article (21814348402) » outstream (22691595903)",
+    "parceiros (10869286) » ne_10 (11238766) » jornal_do_commercio (21268726) » story (22871019115)",
+    "parceiros_mobile (12014326) » ne_10 (21240886) » jornal_do_commercio (53929726)",
+    "parceiros (10869286) » ne_10 (11238766) » jornal_do_commercio (21268726) » especiais (21737885740)",
+    "parceiros (10869286) » ne_10 (11238766) » jornal_do_commercio (21268726) » blog (21725415585)",
+    "parceiros (10869286) » ne_10 (11238766) » jornal_do_commercio (21268726) » blog (21725415585) » meupet (22672700745)",
+    "parceiros (10869286) » ne_10 (11238766) » jornal_do_commercio (21268726) » blog (21725415585) » turismodevalor (22672701000)",
+    "parceiros (10869286) » ne_10 (11238766) » jornal_do_commercio (21268726) » economia (21902175256)",
+    "parceiros (10869286) » ne_10 (11238766) » jornal_do_commercio (21268726) » cultura (21902367665)",
+    "parceiros (10869286) » ne_10 (11238766) » jornal_do_commercio (21268726) » cidades (21902416809)",
+    "parceiros (10869286) » ne_10 (11238766) » jornal_do_commercio (21268726) » mundo (21902418009)",
+    "parceiros (10869286) » ne_10 (11238766) » jornal_do_commercio (21268726) » coluna_turismo_valor (22219888516)"],
+
+    "JC Classico":
+    ["parceiros (10869286) » ne_10 (11238766) » jornal_do_commercio (21268726) » jc_classico (22893389667)", 
+    "parceiros (10869286) » ne_10 (11238766) » jornal_do_commercio (21268726) » jc_classico (22893389667) » anchor (22945567414)"],
+
+    "JC HTML":
+    ["parceiros (10869286) » ne_10 (11238766) » jornal_do_commercio (21268726) » jc_digital (22893451057)",
+     "parceiros (10869286) » ne_10 (11238766) » jornal_do_commercio (21268726) » jc_digital (22893451057) » anchor (22903676133)",
+     "parceiros (10869286) » ne_10 (11238766) » jornal_do_commercio (21268726) » jc_digital (22893451057) » in-article (22893950747)"],
+
+    "Rádio Jornal":
+    ["parceiros (10869286) » ne_10 (11238766) » radio_jornal (21719551231)",
+    "parceiros (10869286) » ne_10 (11238766) » radio_jornal (21719551231) » amp_stories (22722284733)",
+    "parceiros (10869286) » ne_10 (11238766) » radio_jornal (21719551231) » anchor (22834993604)",
+    "parceiros (10869286) » ne_10 (11238766) » radio_jornal (21719551231) » aovivo (21798635724)",
+    "parceiros (10869286) » ne_10 (11238766) » radio_jornal (21719551231) » aovivo (21798635724) » anchor (22835868507)",
+    "parceiros (10869286) » ne_10 (11238766) » radio_jornal (21719551231) » in-article (21814456428)",
+    "parceiros (10869286) » ne_10 (11238766) » radio_jornal (21719551231) » in-article (21814456428) » outstream (22691595906)",
+    "parceiros (10869286) » ne_10 (11238766) » radio_jornal (21719551231) » story (22871014861)"],
+
+    "Social 1":
+    ["parceiros (10869286) » ne_10 (11238766) » jornal_do_commercio (21268726) » blog (21725415585) » social1 (22503128490)",
+    "parceiros (10869286) » ne_10 (11238766) » jornal_do_commercio (21268726) » blog (21725415585) » social1 (22503128490)",
+    "parceiros (10869286) » ne_10 (11238766) » jornal_do_commercio (21268726) » blog (21725415585) » social1 (22503128490) » amp_stories (22722733856)",
+    "parceiros (10869286) » ne_10 (11238766) » jornal_do_commercio (21268726) » blog (21725415585) » social1 (22503128490) » anchor (22835572852)",
+    "parceiros (10869286) » ne_10 (11238766) » jornal_do_commercio (21268726) » blog (21725415585) » social1 (22503128490) » in-article (22503434018)",
+    "parceiros (10869286) » ne_10 (11238766) » jornal_do_commercio (21268726) » blog (21725415585) » social1 (22503128490) » story (22871538047)",
+    "parceiros (10869286) » ne_10 (11238766) » jornal_do_commercio (21268726) » blog (21725415585) » social1 (22503128490)"],
+    
+    "Blog do Torcedor":
+    ["parceiros (10869286) » ne_10 (11238766) » jornal_do_commercio (21268726) » blog (21725415585) » torcedor (22462745544)",
+     "parceiros (10869286) » ne_10 (11238766) » jornal_do_commercio (21268726) » blog (21725415585) » torcedor (22462745544) » amp_stories (22722409933)",
+     "parceiros (10869286) » ne_10 (11238766) » jornal_do_commercio (21268726) » blog (21725415585) » torcedor (22462745544) » anchor (22835339397)",
+     "parceiros (10869286) » ne_10 (11238766) » jornal_do_commercio (21268726) » blog (21725415585) » torcedor (22462745544) » in-article (22473635649)",
+     "parceiros (10869286) » ne_10 (11238766) » jornal_do_commercio (21268726) » blog (21725415585) » torcedor (22462745544) » story (22871018926)", 
+     "parceiros (10869286) » ne_10 (11238766) » jornal_do_commercio (21268726) » blog (21725415585) » torcedor (22462745544) » futebol_na_tv (22958347022)"],
+
+    "TV Jornal":
+    ["parceiros (10869286) » ne_10 (11238766) » tv_jornal (17743246)",
+     "parceiros (10869286) » ne_10 (11238766) » tv_jornal (17743246) » amp_stories (22722750983)",
+     "parceiros (10869286) » ne_10 (11238766) » tv_jornal (17743246) » anchor (22834637230)",
+     "parceiros (10869286) » ne_10 (11238766) » tv_jornal (17743246) » aovivo (21813304676)",
+     "parceiros (10869286) » ne_10 (11238766) » tv_jornal (17743246) » in-article (21814237394)",
+     "parceiros (10869286) » ne_10 (11238766) » tv_jornal (17743246) » story (22871016841)",
+     "parceiros_mobile (12014326) » ne_10 (21240886) » tv_jornal (21818538935)"]
+    }
